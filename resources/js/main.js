3106e1a69bb124caff084b58fa7d61637c530dbd
Ramiro Ching <ramiroching@gmail.com>
2017-12-07 01:13:46 -0800

    add scroll to the main js

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,16 +6,16 @@ $(document).ready(function(){
     autoplaySpeed:2000,
     fade:true
   });
-  // var lastScrollTop = 0;
-  //   $(window).scroll(function(){
-  //     var scrollTop = $(this).scrollTop();
-  //     if (scrollTop - lastScrollTop > 50){
-  //       var navHeight = $('.navbar').css('height');
-  //       $('.navbar').animate({top: '-' + navHeight}, 150);
-  //       lastScrollTop = scrollTop;
-  //     } else if (lastScrollTop - scrollTop > 50) {
-  //       $('.navbar').animate({top: '0px'}, 150);
-  //       lastScrollTop = scrollTop;
-  //     }
-  //   });
+  var lastScrollTop = 0;
+    $(window).scroll(function(){
+      var scrollTop = $(this).scrollTop();
+      if (scrollTop - lastScrollTop > 50){
+        var navHeight = $('.navbar').css('height');
+        $('.navbar').animate({top: '-' + navHeight}, 150);
+        lastScrollTop = scrollTop;
+      } else if (lastScrollTop - scrollTop > 50) {
+        $('.navbar').animate({top: '0px'}, 150);
+        lastScrollTop = scrollTop;
+      }
+    });
 });
