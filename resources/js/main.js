8433bb731b28430f61990ac8f29a7ab40eb7f5de
Ramiro Ching <ramiroching@gmail.com>
2017-12-07 00:01:07 -0800

    completed desktop ver

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
 
-  $('.carousel').slick({
+  $('.slick-track').slick({
     arrows:false,
     autoplay:true,
     autoplaySpeed:2000,
     fade:true
   });
-
+  var lastScrollTop = 0;
+    $(window).scroll(function(){
+      var scrollTop = $(this).scrollTop();
+      if (scrollTop - lastScrollTop > 50){
+        var navHeight = $('.navbar').css('height');
+        $('.navbar').animate({top: '-' + navHeight}, 150);
+        lastScrollTop = scrollTop;
+      } else if (lastScrollTop - scrollTop > 50) {
+        $('.navbar').animate({top: '0px'}, 150);
+        lastScrollTop = scrollTop;
+      }
+    });
 });
